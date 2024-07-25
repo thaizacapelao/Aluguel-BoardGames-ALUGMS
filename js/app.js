e43724c81e924a37ad7e43cdc90b15688e696431
Thaiza Capelão <thaizacapelao@gmail.com>
2024-07-25 17:10:30 -0400

    Adicionando funcoes ao botao

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 function alterarStatus(elementId) {
     let botao = document.getElementById(elementId);
+
     if(botao.classList.contains('dashboard__item__button--return')) {
         botao.classList.remove('dashboard__item__button--return');
-        document.getElementById(elementId).innerHTML = '<label class="texto__paragrafo">Alugar</label>';
+        document.getElementById(elementId).innerHTML = 'Alugar';
     } else{
         botao.classList.add('dashboard__item__button--return');
-        document.getElementById(elementId).innerHTML = '<label class="texto__paragrafo">Devolver</label>';
+        document.getElementById(elementId).innerHTML = 'Devolver';
     }
 }
