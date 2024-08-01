eaf7e4d6d3f0d4e1280eed2379b5ad848a851946
Thaiza Capelão <thaizacapelao@gmail.com>
2024-08-01 15:25:35 -0400

    att ordenacao de numeros

--- js/app.js
+++ js/app.js
@@ -7,11 +7,32 @@ function alterarStatus(gameId) {
             botao.classList.remove('dashboard__item__button--return');
             botao.textContent = 'Alugar';
             imagem.classList.remove('dashboard__item__img--rented');
+            imprimirJogosAlugados();
         }
     } else {
         botao.classList.add('dashboard__item__button--return');
         botao.textContent = 'Devolver';
         imagem.classList.add('dashboard__item__img--rented');
+        imprimirJogosAlugados();
     }
 }
 
+function imprimirJogosAlugados() {
+    let alugados = document.querySelectorAll('.dashboard__item__img--rented').length;
+    console.log(`Total de jogos alugados: ${alugados}`);
+}
+
+// function ordenarNumeros(a, b, c) {
+//     let numeros = [a, b, c];
+//     numeros.sort((x, y) => x - y);
+//     return numeros;
+// }
+
+// function exibirNumerosOrdenados() {
+//     const num1 = parseFloat(document.getElementById('num1').value);
+//     const num2 = parseFloat(document.getElementById('num2').value);
+//     const num3 = parseFloat(document.getElementById('num3').value);
+
+//     const numerosOrdenados = ordenarNumeros(num1, num2, num3);
+//     document.getElementById('resultado').textContent = `Números ordenados: ${numerosOrdenados.join(', ')}`;
+// }
